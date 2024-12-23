2e516f38d5c203f5969151569ed19ccf8f01f2f7
mixxr <michelangelo.serpico@sitecore.com>
2024-12-23 16:20:17 +0100

    init2

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,37 +1,35 @@
 import { User2Icon } from 'lucide-react';
 
-import { PageController, trackUserLoginEvent } from '@sitecore-search/react';
-
 const Login = (): JSX.Element => {
-  const handleChange = (e: { target: { value: string } }): void => {
-    console.log(
-      '[Login] ',
-      e.target.value,
-      PageController.getContext().getUserCustom(),
-      PageController.getContext().getUser(),
-    );
-    // processLogin(e.target.value);
-    // PageController.getContext().setUserGroups(['premium', 'associate']);
-    // trackUserLoginEvent({
-    // email: 'micser_001@example.com',
-    // Optional. User's billing address
-    // address: {
-    //     address_line_1: '123 Main Street',
-    //     address_line_2: 'apartment 123',
-    //     state: 'CA',
-    //     zip: '90210',
-    //     country: 'USA',
-    // },
-    // Optional
-    // gender: 'male',
-    // groups: ['premium', 'associate'],
-    // addn_attributes: {
-    //     email_subscriber: 'True',
-    //     preferred_colors: ['WHITE', 'RED'],
-    //     sub_id: '4321',
-    // }
-    // });
-  };
+  // const handleChange = (e: { target: { value: string } }): void => {
+  //   console.log(
+  //     '[Login] ',
+  //     e.target.value,
+  //     PageController.getContext().getUserCustom(),
+  //     PageController.getContext().getUser(),
+  //   );
+  //   // processLogin(e.target.value);
+  //   // PageController.getContext().setUserGroups(['premium', 'associate']);
+  //   // trackUserLoginEvent({
+  //   // email: 'micser_001@example.com',
+  //   // Optional. User's billing address
+  //   // address: {
+  //   //     address_line_1: '123 Main Street',
+  //   //     address_line_2: 'apartment 123',
+  //   //     state: 'CA',
+  //   //     zip: '90210',
+  //   //     country: 'USA',
+  //   // },
+  //   // Optional
+  //   // gender: 'male',
+  //   // groups: ['premium', 'associate'],
+  //   // addn_attributes: {
+  //   //     email_subscriber: 'True',
+  //   //     preferred_colors: ['WHITE', 'RED'],
+  //   //     sub_id: '4321',
+  //   // }
+  //   // });
+  // };
   return (
     <div className="flex items-center">
       <User2Icon className="text-gray-400 " />
